12e885b193a2513f6a8c7f46228090b7ebfcd0dc
wrcrew@gmail.com <wrcrew@gmail.com>
2016-02-10 20:53:58 -0500

    initialize google map

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
-script.js
+// MODEL -->
 
+
+
+
+
+
+
+
+
+
+
+
+
+// CONTROLLER -->
+
+
+
+
+
+
+
+
+
+
+// VIEW MODEL -->
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.681229, lng: -73.9781},
+    zoom: 15
+  });
+}
